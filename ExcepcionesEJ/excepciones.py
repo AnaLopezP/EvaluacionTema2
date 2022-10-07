937eb6a7a459985945ad31ea962b76fa44014731
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-07 16:56:40 +0200

    cuarto codigo exception

--- ExcepcionesEJ/excepciones.py
+++ ExcepcionesEJ/excepciones.py
@@ -7,17 +7,27 @@ except ZeroDivisionError:
     print("No se puede dividir entre 0")
 
 #segundo codigo:
-lista = [4, 7, 30, 23, 5]
-
-lista[10]
+try:
+    lista = [4, 7, 30, 23, 5]
+    lista[10]
+except IndexError:
+    print("No hay tantos elementos en la lista.")
 
 #tercer codigo:
-paises = { "españa":"español", "eeuu":"inglés", "italia":"italiano" } 
+try:
+    paises = { "españa":"español", "eeuu":"inglés", "italia":"italiano" } 
+    paises["alemania"]
+except KeyError:
+    print("No existe la clave alemania en el diccionario")
 
-paises["alemania"]
 
 #cuarto codigo:
-resultado = "2" + 10
+try:
+    resultado = "2" + 10
+except TypeError:
+    print("No puedes sumar dos tipos de variables distintas.")
+    print("Usa la funcion int() para el 2, y convertirlo así en un numero.")
+
 
 
 
