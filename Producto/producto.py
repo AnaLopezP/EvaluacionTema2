b74905843c463943637c722658a6a6f0a1963dde
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-07 16:24:09 +0200

    getter setter

--- Producto/producto.py
+++ Producto/producto.py
@@ -6,6 +6,30 @@ class Producto:
         self.tipo = tipo
         print("El producto se ha creado con exito")
     
+    def getnombre(self):
+        return self.nombre
+
+    def setnombre(self,nombre):
+        self.nombre = nombre
+
+    def getcodigo(self):
+        return self.codigo
+
+    def setcodigo(self,codigo):
+        self.codigo = codigo
+
+    def getprecio(self):
+        return self.precio
+
+    def setprecio(self, precio):
+        self.precio = precio
+
+    def gettipo(self):
+        return self.tipo
+
+    def settipo(self,tipo):
+        self.tipo = tipo
+
     def __str__(self):
         return("Producto con código: " + str(self.codigo) + ", nombre: "+ str(self.nombre) + ", precio: " + str(self.precio) + " y tipo: " + str(self.tipo))
-        
+
