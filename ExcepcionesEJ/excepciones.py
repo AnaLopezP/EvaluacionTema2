822fb72612129e80e8393ad50f3f45727cc2b26d
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-07 16:50:28 +0200

    pasar arriba

--- ExcepcionesEJ/excepciones.py
+++ ExcepcionesEJ/excepciones.py
@@ -1,5 +1,10 @@
 #primer codigo:
 numero = 7/0
+try:
+    print(numero)
+
+except ZeroDivisionError:
+    print("No se puede dividir entre 0")
 
 #segundo codigo:
 lista = [4, 7, 30, 23, 5]
@@ -14,10 +19,6 @@ paises["alemania"]
 #cuarto codigo:
 resultado = "2" + 10
 
-    
-try:
-    print(numero)
 
-except ZeroDivisionError:
-    print("No se puede dividir entre 0")
+
 
