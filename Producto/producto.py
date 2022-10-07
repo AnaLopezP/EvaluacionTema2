170d21cca32d2abae872562d12b8be56e7b1bcfc
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-07 16:17:59 +0200

    init producto

---
+++ Producto/producto.py
@@ -0,0 +1,8 @@
+class Producto:
+    def __init__(self, codigo, nombre, precio, tipo):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.precio = precio
+        self.tipo = tipo
+        print("El producto se ha creado con exito")
+    
